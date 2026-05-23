94588c94ffc8e851e21e0991c4d4a0689d25edb5
Monti14 <monti14092004@gmail.com>
2026-05-23 16:49:28 +0530

    Fix Terraform LocalStack endpoints

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,13 @@ provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
   region                      = var.aws_region
-  s3_use_path_style           = true
+
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  s3_use_path_style           = true
 
   endpoints {
     ec2 = "http://host.docker.internal:4566"
