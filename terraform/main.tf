59d2f04572a2f753e1f84c2f46d0b7271043385a
Monti14 <monti14092004@gmail.com>
2026-05-23 14:40:35 +0530

    fix localstack networking

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,8 +17,8 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    ec2 = "http://localhost:4566"
-    s3  = "http://localhost:4566"
+    ec2 = "http://host.docker.internal:4566"
+    s3  = "http://host.docker.internal:4566"
   }
 }
 locals {
